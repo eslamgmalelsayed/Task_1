45b6ff87a777ba7955962eec272f2e826bf90d16
eslamgmalelsayed <eslamgmal1@gmail.com>
2024-08-17 16:42:27 +0300

    task1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Task_1';
+  userName: string = 'Eslam';
+  showPara = true;
+  clicksArr: string[] = [];
+  count = 0;
+
+
+  public reset(): void {
+    this.userName = ''
+  }
+  public displayDetails(): void {
+    this.showPara = !this.showPara
+    // this.count++
+    this.clicksArr.push(++this.count + 'click')
+  }
 }
